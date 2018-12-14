abcf1ed528536eaeeb4c2c3b83f2ffb8e1802ab6
Katrin Shibakova <katrina.gubanova@gmail.com>
2018-12-14 18:47:24 +0300

    fixed remarks mentor's to the seventh task (to the personal project)

--- js/filtres.js
+++ js/filtres.js
@@ -7,15 +7,7 @@
   var features = Array.from(document.querySelectorAll('.map__checkbox'));
   var changeFilterCallback = null;
 
-  function checkFeatures(offerFeatures) {
-    return function (item) {
-      return offerFeatures.some(function (offerFeature) {
-        return offerFeature === item;
-      });
-    };
-  }
-
-  function filterDataHandler() {
+  function getFilteredData() {
     var selectsData = selects.reduce(function (obj, elem) {
       obj[elem.name] = elem.value;
       return obj;
@@ -28,28 +20,32 @@
       return item.value;
     });
 
-    var filteredData = window.data.filter(function (elem) {
+    return window.data.filter(function (ad) {
       var priceCheck = true;
       switch (selectsData['housing-price']) {
         case 'middle':
-          priceCheck = elem.offer.price >= 10000 && elem.offer.price <= 50000;
+          priceCheck = ad.offer.price >= 10000 && ad.offer.price <= 50000;
           break;
         case 'low':
-          priceCheck = elem.offer.price < 10000;
+          priceCheck = ad.offer.price < 10000;
           break;
         case 'high':
-          priceCheck = elem.offer.price > 50000;
+          priceCheck = ad.offer.price > 50000;
           break;
       }
 
-      return (elem.offer.type === selectsData['housing-type'] || selectsData['housing-type'] === ANY_VALUE)
+      return (ad.offer.type === selectsData['housing-type'] || selectsData['housing-type'] === ANY_VALUE)
       && priceCheck
-      && (elem.offer.rooms === parseInt(selectsData['housing-rooms'], 10) || selectsData['housing-rooms'] === ANY_VALUE)
-      && (elem.offer.guests === parseInt(selectsData['housing-guests'], 10) || selectsData['housing-guests'] === ANY_VALUE)
-      && selectedValueFeatures.every(checkFeatures(elem.offer.features));
+      && (ad.offer.rooms === parseInt(selectsData['housing-rooms'], 10) || selectsData['housing-rooms'] === ANY_VALUE)
+      && (ad.offer.guests === parseInt(selectsData['housing-guests'], 10) || selectsData['housing-guests'] === ANY_VALUE)
+      && selectedValueFeatures.every(function (item) {
+        return ad.offer.features.includes(item);
+      });
     }).slice(0, 5);
+  }
 
-    changeFilterCallback(filteredData);
+  function filterDataHandler() {
+    changeFilterCallback(getFilteredData());
   }
 
   var debouncedFilerDataHandler = window.general.debounce(filterDataHandler);
